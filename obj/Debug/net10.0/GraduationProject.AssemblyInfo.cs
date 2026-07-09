cf9cbff93cf0cc8db020af1f211c2198d7ff55e4
Michel-magdy <michoo.m.louis@gmail.com>
2026-07-09 21:39:13 +0300

    Update AssemblyInfo and add .gitignore for build artifacts

--- obj/Debug/net10.0/GraduationProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/GraduationProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65236c56be552d8588b8490bed9a3695f0097e1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043d51594d0ca7daabc64fc772f284a7beff8213")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduationProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
